c9dba680b6a3f13f7064557fcba67d95d4c04176
Admin <sriramrgpv84@gmail.com>
2022-08-12 23:55:37 +0530

    hello all to you

---
+++ src/test/java/com/zensar/walmat/WalmatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.walmat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalmatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
